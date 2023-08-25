12092ae1b8ae72ca353c9960d3f06da610bb3120
DaynorTito <107761709+DaynorTito@users.noreply.github.com>
2023-08-25 11:36:29 -0400

    Update decimales.cpp

--- decimales.cpp
+++ decimales.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 stack<int> pila;
-
-
 int main(){
     int ax, bx, dx;
     string cx;
@@ -14,6 +12,10 @@ int main(){
     pila.push(60);
     imprime:
         bx = dx;
+        cout<<
+        dx = pila.top(); pila.pop();
+        pila.push(bx);
+        pila.push(dx);
         bx = bx * 10;
         dx = bx % ax;
         bx = bx / ax;
@@ -23,18 +25,34 @@ int main(){
         bx = bx - 1;
         pila.push(bx);
     if (bx > 0) goto imprime;
-    pila.pop();
-    
-    ax = 0;
-    bx = 0;
-    recorre_cadena:
-        pila.push(cx[ax]);
-        ax = ax + 1;
-    if(ax<cx.length()) goto recorre_cadena;
     while(!pila.empty()){
         cout<<pila.top()<<endl;
         pila.pop();
     }
+    /*
+    pila.pop();
+    ax = 0;
+    bx = 0;
+    recorre_cadena:
+        pila.push(cx[ax]-48);
+        ax = ax + 1;
+    if(ax<cx.length()) goto recorre_cadena;
     
+    ax = 0;
+    bx = 0;
+    dx = 0;
+    cx = "";
+    pop_pila:
+        ax = pila.top(); pila.pop();
+        bx = 0;
+        pop2:
+            
+            
+            bx = bx + 1;
+        if(bx<cx.length()) goto pop2;
+    if(!pila.empty()) goto pop_pila;*/
     return 0;
 }
+
+
+
