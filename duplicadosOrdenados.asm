fa5cbd884ae991726e66026279bd6fdd8f44475b
DaynorTito <107761709+DaynorTito@users.noreply.github.com>
2023-11-27 16:01:56 +0000

    Update duplicadosOrdenados.asm

--- duplicadosOrdenados.asm
+++ duplicadosOrdenados.asm
@@ -1,15 +1,12 @@
 ; multi-segment executable file template.
 
 data segment
-    ; add your data here!
-    pkey db "press any key...$"
-    vc db 0, 0, 0
-    v db 0, 0, 0, 0, 0, 0, 0, 0, 0
-    vf db "$" 
-    vec db 0 dup(100)
+    ve db 0,0,0,0,0,0,0,0,0,0
+    vc db 0,0,0,0,0,0,0,0,0,0
+    ocu db 0 dup(100)
+    
     enter db 10, 13, 36
     num db 0
-    
 ends
 
 stack segment
@@ -17,24 +14,19 @@ stack segment
 ends
 
 code segment
-start:
-; set segment registers:
+start:                   
+
     mov ax, data
     mov ds, ax
     mov es, ax
     
-    mov si, offset v
-    jmp leer
-    nuevo:
-    mov si, offset vec
-    mov si, bx
-    cmp [si], 1
-    jz leer
-    push bx
-    mov [si], 1
-    mov si, offset num
-    inc [si]
-    leer:
+    mov di, offset vc
+    mov si, offset ve
+    
+    
+    
+    
+    otro:
     mov ax, 0
     mov cx, 0
     mov bx, 0
@@ -43,10 +35,10 @@ start:
     mov ax, 0
     mov ah, 1
     int 21h
-    cmp al, 13
-    jz ciclosd
     cmp al, 32
-    jz nuevo
+    jz ciclosd
+    cmp al, 13
+    jz finlec
     mov ah, 0
     sub ax, 30h
     mov cx, ax     
@@ -57,122 +49,194 @@ start:
     add bx, cx
     jmp ciclo
     ciclosd:
-    mov si, offset vec
-    mov si, bx
-    cmp [si], 1
-    jz siguiente
-    mov [si], 1    
-    push bx
-    mov si, offset num
-    inc [si]
-    siguiente:
-    mov si, offset num
-    mov ax, 0
-    mov cx, 0
-    mov cl, [si]
-    mov bx, cx
-    mov [si], 0
-    
-    mov si, offset v
-    copiar:
-    pop ax
-    mov [si], al
-    inc si
-    loop copiar
-    limpiar:
-    mov [si], 0
-    inc si
-    cmp [si], "$"
-    jz salir
-    jmp limpiar
-    salir:
+    mov [si], bl 
 
+    inc si
+    inc [di]
+    jmp otro
+    finlec: 
+    mov [si], bl
+    inc si
+    inc [di]
+    
+    mov cx, 0
+    mov cl,[di]
+    push cx
+    mov [di], 0
+    
+    
     lea dx, enter
     mov ah, 9
-    int 21h
-    
-    mov ax, 0
-    mov bx, 0
-    mov cx, 0
-    mov dx, 0
-    
-     
-    lea si, v
-    cicloo:
-    
-    mov di, si 
-    inc di
-    ciclo2:
-    mov bl, [si]
-    mov bh, [di]
-    cmp bh, bl
-    jc cambiar
-    jmp continuar
-    cambiar:
-    mov [si], bh
-    mov [di], bl
-    continuar:
-    inc di
-    cmp [di], "$"
-    jz sigE
-    jmp ciclo2
-    
-    sigE:
-    add si, 2
-    cmp [si], "$"
-    jz finordenamiento
-    dec si
-    jmp cicloo
-    finordenamiento: 
-     
-     
-     
-     
-    mov cx, 0
-    mov bx, 0
-    mov ax, 0
-    mov dx, 0
-    
-    lea si, v
-    mostrar:
-    
-    mov dl, [si]
-    cmp dl, 0
-    jz sig
-    cmp dl, 10
-    jc men
-    mov ax, dx
-    mov dx, 0
-    mov bx, 10
-    div bx
-    mov bx, dx
-    add al, 30h
-    mov ah, 2
-    mov dh, 0
-    mov dl, al
-    int 21h
-    mov dx, bx
-    men:
-    
-    add dl, 30h
-    mov ah, 2
-    mov dh, 0
-    int 21h
-    
-    mov al, 32
-    mov ah, 2
-    mov dh, 0
-    mov dl, al
-    int 21h
-    sig:    
-    inc si
-    cmp [si], "$"
-    jz fin
-    jmp mostrar
-    fin:      
+    int 21h      
             
+            
+            
+            
+            
+            
+    mov di, offset vc
+    otro2:
+    mov ax, 0
+    mov cx, 0
+    mov bx, 0
+    mov dx, 1
+    ciclo2:
+    mov ax, 0
+    mov ah, 1
+    int 21h
+    cmp al, 32
+    jz ciclosd2
+    cmp al, 13
+    jz finlec2
+    mov ah, 0
+    sub ax, 30h
+    mov cx, ax     
+    mov ax, bx
+    mov dx, 10
+    mul dx
+    mov bx, ax
+    add bx, cx
+    jmp ciclo2
+    ciclosd2:
+    mov [di], bl
+    inc di
+    jmp otro2
+    finlec2: 
+    mov [di], bl
+    inc di   
+    mov ax, 0
+    mov cx, 0
+    mov bx, 0
+    mov dx, 0
+    
+    pop cx
+    
+    mov si, offset num
+    mov [si], cl  
+    mov si, offset ve
+    mov di, offset vc
+    
+    add di, cx
+    
+    dec di
+    mov ax, 0
+    mov bx, 0
+    push bx
+    mov dx, 0 
+    
+    multip: 
+    mov al, [si]
+    inc si
+    mov bl, [di]
+    dec di
+    mul bx
+    mov bx, cx
+    pop cx
+    add cx, ax
+    push cx
+    mov ax, 0
+    mov cx, bx
+    loop multip
+    
+    mov al, [si]
+    inc si
+    mov bl, [di]
+    dec di
+    mul bx
+    mov bx, cx
+    pop cx
+    add cx, ax
+    push cx
+    mov ax, 0
+    mov cx, bx
+    
+    pop bx
+    ; -----------
+    lea dx, enter
+    mov ah, 9       
+    int 21h
+    
+    mov ax, 0
+    mov cx, 0
+    mov dx, 0
+    
+    mov ax, bx
+    mov bx, ax
+    mov cx, bx
+    mov si, offset num
+    mov [si], 0
+    ciclosdd:
+      mov dx, 0
+      mov ax, bx
+      mov bx, 10
+      cmp ax, 10
+      jc menor
+      div bx
+      inc [si]
+      mov bx, ax
+      add dl, 48
+      mov bx, ax
+      mov dh, 0
+      push dx
+    loop ciclosdd
+    
+    
+    menor:
+    add al, 48 
+    mov ah, 0
+    push ax
+    inc [si]
+    mov cl, [si]
+    
+    
+    mostrar3: 
+    mov ax, 0
+    pop ax
+    mov ah, 2
+    mov dh, 0
+    mov dl, al
+    int 21h
+    loop mostrar3
+    
+    
+    mov si, offset num
+    mov ch, 0
+    mov cl, [si]
+    mov si, offset ve
+    mov di, offset ocu
+    conta:  
+    mov di, [si]
+    inc [di]
+    inc si
+    loop conta
+    
+    mov si, offset num
+    mov ch, 0
+    mov cl, [si]
+    mov si, offset vc
+    mov di, offset ocu
+    contas:  
+    mov di, [si]
+    inc [di]
+    inc si
+    loop contas
+    
+    mov di, offset ocu
+    mov cx, 100
+    mov bx, 0
+    buscar:
+    cmp [di], 0
+    jz sige
+    jmp igu
+    igu:
+    mov bl, [di]
+    sige:     
+    inc di
+    loop buscar
+     
+     
     mov ax, 4c00h ; exit to operating system.
-    int 21h    
+        int 21h    
 ends
 
 end start ; set entry point and stop the assembler.
